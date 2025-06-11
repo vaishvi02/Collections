197f48bbb8e78a960c5841972ce2ec0cb7771383
vaishvi02 <vaishvimathur@gmail.com>
2025-06-11 14:26:56 +0530

    urlify

--- src/main/java/StringsAndArrays/replaceVariables.java
+++ src/main/java/StringsAndArrays/replaceVariables.java
@@ -5,14 +5,17 @@ public class replaceVariables {
     public static String urlify(String url) {
         StringBuffer stringBuffer = new StringBuffer();
         String[] strArr = url.trim().split(" ");
-        for (String s : strArr) {
-            stringBuffer.append(s).append("%20");
+        for (int i=0; i<strArr.length; i++) {
+            if(i != strArr.length-1)
+                stringBuffer.append(strArr[i]).append("%20");
+            else
+                stringBuffer.append(strArr[i]);
         }
         return stringBuffer.toString();
     }
 
     public static void main(String[] args) {
-        String s = "Hello John     ";
+        String s = "Hello John    Welcome ";
         System.out.println("Replaced: " + urlify(s));
     }
 }
