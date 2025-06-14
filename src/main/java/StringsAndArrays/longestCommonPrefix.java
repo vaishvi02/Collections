656c0fd6cf6269e540e11c3afadc89a41b2e9beb
vaishvi02 <vaishvimathur@gmail.com>
2025-06-14 12:36:44 +0530

    longest common prefix

---
+++ src/main/java/StringsAndArrays/longestCommonPrefix.java
@@ -0,0 +1,21 @@
+package main.java.StringsAndArrays;
+
+public class longestCommonPrefix {
+    public static String findLongestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.length() == 0)
+                    return "";
+            }
+        }
+
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        String[] strs = { "dog", "dracecar", "docar" };
+        System.out.println(findLongestCommonPrefix(strs));
+    }
+}
