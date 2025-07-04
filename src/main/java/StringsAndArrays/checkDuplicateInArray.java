dc12cb9004d97af191ba678b73a259f2a113abce
vaishvi02 <vaishvimathur@gmail.com>
2025-07-04 15:34:58 +0530

    duplicate

---
+++ src/main/java/StringsAndArrays/checkDuplicateInArray.java
@@ -0,0 +1,26 @@
+package main.java.StringsAndArrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class checkDuplicateInArray {
+    public static boolean containsDuplicate(int[] nums) {
+
+        Set<Integer> hs = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(!hs.contains(nums[i])){
+                hs.add(nums[i]);
+            }
+            else
+                return true;
+
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        System.out.println("is duplicate: "+containsDuplicate(nums));
+    }
+
+}
