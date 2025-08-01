8c29b636a8cbc1dd139b2070f71dda0a216d7c5f
Vikash <yequalstovikash@gmail.com>
2025-08-01 08:28:56 +0530

    leetcode practice

--- src/main/java/LeetCodeBlind75/ValidParanthesis.java
+++ src/main/java/LeetCodeBlind75/ValidParanthesis.java
@@ -1,9 +1,34 @@
 package main.java.LeetCodeBlind75;
 
+import java.util.Stack;
+
 public class ValidParanthesis {
 
     public boolean checkValidParanthesis(String str) {
 
-        return false;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(' || str.charAt(i) == '{' || str.charAt(i) == '[') {
+                stack.push(str.charAt(i));
+            } else {
+                if ((!stack.isEmpty() || stack.size() != 0)) {
+                    if ((str.charAt(i) == ')' && stack.peek().equals('(')) ||
+                            (str.charAt(i) == '}' && stack.peek().equals('{')) ||
+                            (str.charAt(i) == ']') && stack.peek().equals('[')) {
+
+                        {
+                            stack.pop();
+                        }
+                    }
+                } else
+                    return false;
+            }
+        }
+        return stack.empty();
+    }
+
+    public static void main(String[] args) {
+        ValidParanthesis vp = new ValidParanthesis();
+        System.out.println(vp.checkValidParanthesis("(]})"));
     }
 }
